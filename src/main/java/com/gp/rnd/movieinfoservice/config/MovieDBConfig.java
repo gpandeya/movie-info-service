c410d9963a98be9851ee822e95caa3ccbe01bb37
Pandeya, Gyanesh <Gyanesh.Pandeya@McKesson.com>
2020-02-12 23:51:36 -0800

    multiple changes

--- src/main/java/com/gp/rnd/movieinfoservice/config/MovieDBConfig.java
+++ src/main/java/com/gp/rnd/movieinfoservice/config/MovieDBConfig.java
@@ -1,10 +1,12 @@
 package com.gp.rnd.movieinfoservice.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties("moviedb")
+@RefreshScope
 public class MovieDBConfig {
 
 	private String baseUrl;
--- src/main/java/com/gp/rnd/movieinfoservice/controller/MovieInfoController.java
+++ src/main/java/com/gp/rnd/movieinfoservice/controller/MovieInfoController.java
@@ -2,6 +2,7 @@ package com.gp.rnd.movieinfoservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import com.gp.rnd.movieinfoservice.entity.MovieSummary;
 
 @RestController
 @RequestMapping("/movies")
+@RefreshScope
 public class MovieInfoController {
 	
 	
